f11b5f190b6a7346234b854576d60b0e015fef9b
SalilShenoy <salilshenoy@gmail.com>
2016-12-04 19:07:44 -0800

    misc files

--- scrapper.php
+++ scrapper.php
@@ -1,14 +1,14 @@
 <!-- Scrapper in PHP -->
 <?php
     
-    $fp = gzopen("lexicon.txt.gz", "r");
+    $fp = gzopen("lexicon.txt.gz","r");
     if ($fp == null) {
         print ("No such file");
         exit(0);
     }
 
     while ($line = gzgets($fp)) {
-        $line = ltrim($line, '/');
 	$line = preg_replace('/[+<>]+/',' ',$line);
+	print $line;
     }
 ?>
